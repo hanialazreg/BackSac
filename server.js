fa7080bcd6ddd0b1cf0b7ffd1d0d4c002668efbf
hanialazreg <lazreghania@gmail.com>
2020-05-04 01:17:03 +0100

    data base setting not final file

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
 const express = require("express");
 
 app = express();
 
-const port = 3000;
+// middelware-------------------
+app.use(express.json());
+
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE_CONNEXION.replace(
+  "<PASSWORD>",
+  process.env.PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log(" database connected"));
+
+const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`server is listening in port ${port}`));
